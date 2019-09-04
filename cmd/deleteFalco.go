844e17b6862f3589641ce065e14892cfd03fae15
Kris Nova <kris@nivenly.com>
2019-09-04 09:29:18 -0700

    Rebuilding git logs all the live long day
    Signed-off-by: Kris Nova <kris@nivenly.com>

---
+++ cmd/deleteFalco.go
@@ -0,0 +1,73 @@
+/*
+Copyright © 2019 Kris Nova <kris@nivenly.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	kubernetesfalc "github.com/kris-nova/falcoctl/kubernetes"
+	"github.com/kubicorn/kubicorn/pkg/cli"
+	"github.com/kubicorn/kubicorn/pkg/local"
+	"github.com/kubicorn/kubicorn/pkg/logger"
+
+	"github.com/spf13/cobra"
+)
+
+// deleteFalcoCmd represents the deleteFalco command
+var (
+	deleteFalcoCmd = &cobra.Command{
+		Use:   "falco",
+		Short: "Delete Falco from Kubernetes",
+		Long:  ``,
+		Run: func(cmd *cobra.Command, args []string) {
+			err, exitCode := DeleteFalcoEntry(i, kubeConfigPath)
+			if err != nil {
+				logger.Critical("Fatal error: %v", err)
+				os.Exit(exitCode)
+			}
+			logger.Always("Success.")
+			///	os.Exit(1)		},
+		},
+	}
+)
+
+var (
+	// Global for all install methods
+	i              = &kubernetesfalc.FalcoInstaller{}
+	kubeConfigPath string
+)
+
+func init() {
+	deleteCmd.AddCommand(deleteFalcoCmd)
+	installFalcoCmd.Flags().StringVarP(&kubeConfigPath, "kube-config-path", "k",
+		cli.StrEnvDef("FALCOCTL_KUBE_CONFIG_PATH", fmt.Sprintf("%s/.kube/config", local.Home())),
+		"Set the path to the Kube config")
+	installFalcoCmd.Flags().StringVarP(&i.NamespaceName, "namespace", "n",
+		cli.StrEnvDef("FALCOCTL_KUBE_NAMESPACE", "falco"), "Set the namespace to install Falco in")
+}
+
+func DeleteFalcoEntry(installer *kubernetesfalc.FalcoInstaller, kubeConfigPath string) (error, int) {
+	k8s, err := kubernetesfalc.NewK8sFromKubeConfigPath(kubeConfigPath)
+	if err != nil {
+		return fmt.Errorf("unable to parse kube config: %v", err), 98
+	}
+	err = installer.Delete(k8s)
+	if err != nil {
+		return fmt.Errorf("unable to delete falco in Kubernetes: %v", err), 99
+	}
+	return nil, 0
+}
---
+++ kubernetes/audit.go
@@ -0,0 +1,114 @@
+package kubernetesfalc
+
+import (
+	"fmt"
+	"strings"
+
+	"k8s.io/api/auditregistration/v1alpha1"
+
+	"github.com/kris-nova/logger"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	APIServerNamespace = "kube-system"
+)
+
+type AuditInstaller struct {
+	k8s            *kubernetesConfigClient
+	FalcoNamespace string
+}
+
+func (i *AuditInstaller) Install(k8s *kubernetesConfigClient) error {
+	i.k8s = k8s
+	logger.Info("Configuring Kubernetes Audit Logging")
+	logger.Info("Searching for API server...")
+	pods, err := k8s.client.CoreV1().Pods(APIServerNamespace).List(metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("unable to list pods in kube-system Namespace: %v", err)
+	}
+	var apiServer v1.Pod
+	found := false
+	for _, pod := range pods.Items {
+		if strings.Contains(pod.Name, "kube-apiserver") {
+			found = true
+			logger.Success("Found API server...")
+			apiServer = pod
+			break
+		}
+	}
+	if !found {
+		return fmt.Errorf("unable to find kube-apiserver")
+	}
+
+	// -----------------------------------------------------------------------------------------------------------------
+	//
+	// Enable Audit Flags
+	logger.Info("Setting new API server flags...")
+	apiServer.Spec.Containers[0].Command = append(apiServer.Spec.Containers[0].Command,
+		"--tls-private-key-file=/etc/kubernetes/pki/apiserver.key")
+	apiServer.Spec.Containers[0].Command = append(apiServer.Spec.Containers[0].Command,
+		"--feature-gates=DynamicAuditing=true")
+	apiServer.Spec.Containers[0].Command = append(apiServer.Spec.Containers[0].Command,
+		"--runtime-config=auditregistration.k8s.io/v1alpha1=true")
+
+	//_, err = i.k8s.client.CoreV1().Pods(APIServerNamespace).Update(&apiServer)
+	//if err != nil {
+	//	return fmt.Errorf("unable to update API server: %v", err)
+	//}
+
+	// TODO - Generate YAML and patch the server manifest
+
+	logger.Success("Successfully updated API server...")
+	_, err = i, k8s.client.CoreV1().Pods(APIServerNamespace).Delete(apiServer.Name, &metav1.DeleteOptions{})
+	if err != nil {
+		return fmt.Errorf("unable to restart API server: %v", err)
+	}
+	logger.Success("Audit logging enabled")
+	logger.Info("Configuring Falco as an audit endpoint")
+
+	svc, err := i.k8s.client.CoreV1().Services(i.FalcoNamespace).Get("falco-service", metav1.GetOptions{})
+	if err != nil {
+		return fmt.Errorf("Unable to find Falco service")
+	}
+	ip := svc.Spec.ClusterIP
+	endpoint := fmt.Sprintf("http://%s:8765/k8s_audit", ip)
+	as := v1alpha1.AuditSink{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "falco-audit-sink",
+		},
+		Spec: v1alpha1.AuditSinkSpec{
+			Policy: v1alpha1.Policy{
+				Level: "RequestResponse",
+				Stages: []v1alpha1.Stage{
+					"ResponseComplete",
+					"ResponseStarted",
+				},
+			},
+			Webhook: v1alpha1.Webhook{
+				Throttle: &v1alpha1.WebhookThrottleConfig{
+					QPS:   i64(10),
+					Burst: i64(15),
+				},
+				ClientConfig: v1alpha1.WebhookClientConfig{
+					URL: &endpoint,
+				},
+			},
+		},
+	}
+
+	_, err = i.k8s.client.AuditregistrationV1alpha1().AuditSinks().Create(&as)
+	if err != nil {
+		return fmt.Errorf("unable to instlal AuditSink: %v", err)
+	}
+	logger.Success("Kubernetes AuditSink enabled with Falco.")
+	return nil
+}
+
+func i64(i int) *int64 {
+	i64 := int64(i)
+	return &i64
+}
