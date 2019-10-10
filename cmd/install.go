c0c5455233cb54e3a887b5133ab4ad407c6af6e0
Leonardo Di Donato <leodidonato@gmail.com>
2019-10-10 18:43:08 +0200

    update(cmd): revamp install command
    Co-authored-by: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- cmd/install.go
+++ cmd/install.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2019 Kris Nova <kris@nivenly.com>
+Copyright © 2019 The Falco Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,26 +13,44 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package cmd
 
 import (
+	"github.com/falcosecurity/falcoctl/pkg/kubernetes/factory"
 	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
-// installCmd represents the install command
-var installCmd = &cobra.Command{
-	Use:   "install",
-	Short: "Install a component wih falcoctl",
-	Long:  ``,
-	//Run: func(cmd *cobra.Command, args []string) {
-	//	fmt.Println("install called")
-	//},
+// InstallOptions represents the install command options
+type InstallOptions struct {
+	genericclioptions.IOStreams
 }
 
-func init() {
-	rootCmd.AddCommand(installCmd)
+// Validate validates the `install` command options
+func (o InstallOptions) Validate(c *cobra.Command, args []string) error {
+	return nil
+}
 
-	installCmd.AddCommand(installFalcoCmd)
-	//installCmd.AddCommand(installOutputCmd)
-	//installCmd.AddCommand(installRuleCmd)
+// NewInstallOptions instantiates the install command options
+func NewInstallOptions(streams genericclioptions.IOStreams) CommandOptions {
+	return &InstallOptions{
+		IOStreams: streams,
+	}
+}
+
+// NewInstallCommand creates the install command
+func NewInstallCommand(streams genericclioptions.IOStreams, f factory.Factory) *cobra.Command {
+	// o := NewInstallOptions(streams).(*InstallOptions)
+
+	cmd := &cobra.Command{
+		Use:                   "install",
+		DisableFlagsInUseLine: true,
+		Short:                 "Install a component wih falcoctl",
+		Long:                  `Install a component wih falcoctl`,
+	}
+
+	cmd.AddCommand(NewFalcoInstallCommand(streams, f))
+
+	return cmd
 }
