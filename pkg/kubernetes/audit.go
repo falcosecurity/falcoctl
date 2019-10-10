4f5c4d63bc72cf27dae94d91a231854c4e11108b
Leonardo Di Donato <leodidonato@gmail.com>
2019-10-10 18:42:12 +0200

    update(pkg/kubernetes): falco and audit installer improvements
    Co-authored-by: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ pkg/kubernetes/audit.go
@@ -0,0 +1,147 @@
+/*
+Copyright © 2019 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kubernetesfalc
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/kris-nova/logger"
+	"k8s.io/api/auditregistration/v1alpha1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	auditregistrationv1alpha1 "k8s.io/client-go/kubernetes/typed/auditregistration/v1alpha1"
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/utils/pointer"
+)
+
+const (
+	// APIServerNamespace ...
+	APIServerNamespace = "kube-system"
+)
+
+type auditInstaller struct {
+	coreClient         corev1.CoreV1Interface
+	auditClient        auditregistrationv1alpha1.AuditregistrationV1alpha1Interface
+	namespace          string
+	serviceAccountName string
+}
+
+// NewAuditInstaller ...
+func NewAuditInstaller(coreClient corev1.CoreV1Interface, auditClient auditregistrationv1alpha1.AuditregistrationV1alpha1Interface, namespace string, serviceAccountName string) *auditInstaller {
+	return &auditInstaller{
+		coreClient:  coreClient,
+		auditClient: auditClient,
+	}
+}
+
+// Install ...
+func (i *auditInstaller) Install() error {
+	logger.Always("Configuring Kubernetes Audit Logging")
+	logger.Always("Searching for API server...")
+	pods, err := i.coreClient.Pods(APIServerNamespace).List(metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+	var apiServer v1.Pod
+	found := false
+	for _, pod := range pods.Items {
+		if strings.Contains(pod.Name, "kube-apiserver") {
+			found = true
+			logger.Always("Found API server...")
+			apiServer = pod
+			break
+		}
+	}
+	if !found {
+		return fmt.Errorf("Unable to find kube-apiserver")
+	}
+
+	// Enable audit flags
+	logger.Always("Setting new API server flags...")
+	apiServer.Spec.Containers[0].Command = append(
+		apiServer.Spec.Containers[0].Command,
+		"--tls-private-key-file=/etc/kubernetes/pki/apiserver.key",
+	)
+	apiServer.Spec.Containers[0].Command = append(
+		apiServer.Spec.Containers[0].Command,
+		"--feature-gates=DynamicAuditing=true",
+	)
+	apiServer.Spec.Containers[0].Command = append(
+		apiServer.Spec.Containers[0].Command,
+		"--runtime-config=auditregistration.k8s.io/v1alpha1=true",
+	)
+
+	_, err = i.coreClient.Pods(APIServerNamespace).Update(&apiServer)
+	if err != nil {
+		return fmt.Errorf("unable to update API server: %v", err)
+	}
+
+	// TODO - Generate YAML and patch the server manifest
+	// We have to replace the YAML via SSH - we CANNOT update the static pod.
+	// The Kubelet is smart enough to restart the API server pod once the new configuration has been written.
+	//
+	// - Generate YAML from struct
+	// - SSH / SCP the file to /etc/kubernetes
+	// - Validate the AuditSink resource is available
+
+	logger.Always("Successfully updated API server...")
+	err = i.coreClient.Pods(APIServerNamespace).Delete(apiServer.Name, &metav1.DeleteOptions{})
+	if err != nil {
+		return fmt.Errorf("unable to restart API server: %v", err)
+	}
+	logger.Always("Audit logging enabled")
+	logger.Always("Configuring Falco as an audit endpoint")
+
+	svc, err := i.coreClient.Services(i.namespace).Get(i.serviceAccountName, metav1.GetOptions{})
+	if err != nil {
+		return fmt.Errorf("Unable to find Falco service")
+	}
+	ip := svc.Spec.ClusterIP
+	endpoint := fmt.Sprintf("http://%s:8765/k8s_audit", ip)
+	as := &v1alpha1.AuditSink{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "falco-audit-sink",
+		},
+		Spec: v1alpha1.AuditSinkSpec{
+			Policy: v1alpha1.Policy{
+				Level: "RequestResponse",
+				Stages: []v1alpha1.Stage{
+					"ResponseComplete",
+					"ResponseStarted",
+				},
+			},
+			Webhook: v1alpha1.Webhook{
+				Throttle: &v1alpha1.WebhookThrottleConfig{
+					QPS:   pointer.Int64Ptr(10),
+					Burst: pointer.Int64Ptr(15),
+				},
+				ClientConfig: v1alpha1.WebhookClientConfig{
+					URL: &endpoint,
+				},
+			},
+		},
+	}
+
+	_, err = i.auditClient.AuditSinks().Create(as)
+	if err != nil {
+		return err
+	}
+	logger.Always("Kubernetes AuditSink enabled with Falco.")
+	return nil
+}
--- pkg/kubernetes/falco.go
+++ pkg/kubernetes/falco.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2019 Kris Nova <kris@nivenly.com>
+Copyright © 2019 The Falco Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,41 +17,77 @@ limitations under the License.
 package kubernetesfalc
 
 import (
-	"fmt"
-
+	"github.com/falcosecurity/falcoctl/pkg/kubernetes/factory"
 	"github.com/kris-nova/logger"
 	"k8s.io/api/apps/v1beta2"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/api/rbac/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	appsv1beta2 "k8s.io/client-go/kubernetes/typed/apps/v1beta2"
+	auditregistrationv1alpha1 "k8s.io/client-go/kubernetes/typed/auditregistration/v1alpha1"
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	rbacv1beta1 "k8s.io/client-go/kubernetes/typed/rbac/v1beta1"
+	"k8s.io/utils/pointer"
 )
 
-// FalcoInstaller is a data structure used to install Falco in Kubernetes
-type FalcoInstaller struct {
-	k8s *kubernetesConfigClient
+// falcoInstaller is a data structure used to install Falco in Kubernetes
+type falcoInstaller struct {
+	coreClient        corev1.CoreV1Interface
+	auditClient       auditregistrationv1alpha1.AuditregistrationV1alpha1Interface
+	rbacClient        rbacv1beta1.RbacV1beta1Interface
+	appsv1beta2Client appsv1beta2.AppsV1beta2Interface
+	namespace         string
+}
 
-	// NamespaceName is an example of a "Global" variable we certainly should have
-	NamespaceName string
+// FalcoInstaller ...
+type FalcoInstaller interface {
+	Install() error
+	Delete() error
+}
 
-	// DaemonSetName is an example of a "Possible" variable we don't know if we wan't to expose
-	DameonSetName string
+// NewFalcoInstaller creates a new Falco installer for Kubernetes
+func NewFalcoInstaller(f factory.Factory) (FalcoInstaller, error) {
+	i := &falcoInstaller{}
+	restConfig, err := f.ToRESTConfig()
+	if err != nil {
+		logger.Critical("Fatal error: %v", err)
+		return nil, err
+	}
+	i.coreClient, err = corev1.NewForConfig(restConfig)
+	if err != nil {
+		logger.Critical("Fatal error: %v", err)
+		return nil, err
+	}
+	i.rbacClient, err = rbacv1beta1.NewForConfig(restConfig)
+	if err != nil {
+		logger.Critical("Fatal error: %v", err)
+		return nil, err
+	}
+	i.appsv1beta2Client, err = appsv1beta2.NewForConfig(restConfig)
+	if err != nil {
+		logger.Critical("Fatal error: %v", err)
+		return nil, err
+	}
+	i.auditClient, err = auditregistrationv1alpha1.NewForConfig(restConfig)
+	if err != nil {
+		logger.Critical("Fatal error: %v", err)
+		return nil, err
+	}
+	i.namespace = "falco-ns" // todo > inject
+
+	return i, nil
 }
 
 // Delete is brute force method used to permanently destroy Falco resources in Kubernetes.
-func (i *FalcoInstaller) Delete(k8s *kubernetesConfigClient) error {
-	i.k8s = k8s
-
-	// -----------------------------------------------------------------------------------------------------------------
-	//
+// todo > delete also service account, etc.
+func (i *falcoInstaller) Delete() error {
 	// Namespace
-	err := i.k8s.client.CoreV1().Namespaces().Delete(i.NamespaceName, &metav1.DeleteOptions{})
+	err := i.coreClient.Namespaces().Delete(i.namespace, &metav1.DeleteOptions{})
 	if err != nil {
-		logger.Info("Delete Namespace: %v", err)
+		logger.Critical("Error deleting namespace: %v", err)
 	} else {
-		logger.Success("Delete Falco Namespace [%s]", i.NamespaceName)
+		logger.Always("Falco namespace deleted: %s", i.namespace)
 	}
-
 	return nil
 }
 
@@ -59,74 +95,60 @@ func (i *FalcoInstaller) Delete(k8s *kubernetesConfigClient) error {
 // and move on with installing the others.
 // Right now, the methods to obtain the Kubernetes resources are hard-coded here, but I (@kris-nova) think we should
 // abstract the logic for how we acquire the description for our resources.
-func (i *FalcoInstaller) Install(k8s *kubernetesConfigClient) error {
-	// Strong preference over keeping the Kubernetes client only available in this package, but still allowing an
-	// outside package to use this based on logic from the unexported kubernetesConfigClient
-	i.k8s = k8s
-
-	// -----------------------------------------------------------------------------------------------------------------
-	//
+func (i *falcoInstaller) Install() error {
 	// Namespace
-	err := i.iNamespace()
+	err := createNamespace(i.coreClient.Namespaces(), i.namespace)
 	if err != nil {
-		logger.Info("Create Namespace: %v", err)
-	} else {
-		logger.Success("Installed Falco Namespace [%s]", i.NamespaceName)
+		logger.Critical("Error creating namespace: %v", err)
+		return err
 	}
+	logger.Always("Falco namespace created: %s", i.namespace)
 
-	// -----------------------------------------------------------------------------------------------------------------
-	//
-	// Auth
-	err = i.iFalcoRBAC()
+	// RBAC
+	serviceAccountName := "falco-sa" // todo > inject
+	err = createRBAC(i.coreClient.ServiceAccounts(i.namespace), i.rbacClient.ClusterRoles(), i.rbacClient.ClusterRoleBindings(), serviceAccountName, i.namespace)
 	if err != nil {
-		logger.Info("Create RBAC: %v", err)
-	} else {
-		logger.Success("Installed Falco RBAC")
+		logger.Critical("Error creating service account: %v", err)
+		return err
 	}
+	logger.Always("Falco service accounts created: %s", i.namespace)
 
-	// -----------------------------------------------------------------------------------------------------------------
-	//
 	// ConfigMap
-	err = i.iConfigMap()
+	configMapName := "falco-cm" // todo > inject
+	err = createConfigMap(i.coreClient.ConfigMaps(i.namespace), configMapName, i.namespace)
 	if err != nil {
-		logger.Info("Create ConfigMap: %v", err)
-	} else {
-		logger.Success("Installed Falco ConfigMap")
+		logger.Critical("Error creating config map: %v", err)
+		return err
 	}
+	logger.Always("Falco config map created: %s", i.namespace)
 
-	// -----------------------------------------------------------------------------------------------------------------
-	//
 	// DaemonSet
-	err = i.iDaemonSet(false)
+	daemonSetName := "falco-ds" // todo > inject
+	err = createDaemonSet(i.appsv1beta2Client.DaemonSets(i.namespace), serviceAccountName, configMapName, daemonSetName, false)
 	if err != nil {
-		logger.Info("Create Falco DaemonSet: %v", err)
-	} else {
-		logger.Success("Installed Falco DameonSet [%s]", i.DameonSetName)
+		logger.Critical("Error creating daemon set: %v", err)
+		return err
 	}
+	logger.Always("Falco daemon set created: %s", i.namespace)
 
-	// -----------------------------------------------------------------------------------------------------------------
-	//
 	// Service
-	err = i.iFalcoService()
+	serviceName := "falco-svc" // todo > inject
+	err = createService(i.coreClient.Services(i.namespace), serviceName, i.namespace)
 	if err != nil {
-		logger.Info("Create Falco Service: %v", err)
-	} else {
-		logger.Success("Installed Falco Service [%s]", "falco-service")
+		logger.Critical("Error creating service: %v", err)
+		return err
 	}
+	logger.Always("Falco service created: %s", i.namespace)
 
-	audit := AuditInstaller{
-		FalcoNamespace: i.NamespaceName,
-	}
-	return audit.Install(i.k8s)
-
+	return NewAuditInstaller(i.coreClient, i.auditClient, i.namespace, serviceAccountName).Install()
 }
 
-func (i *FalcoInstaller) iFalcoService() error {
-	svc := v1.Service{
+func createService(serviceClient corev1.ServiceInterface, serviceName string, namespace string) error {
+	svc := &v1.Service{
 		TypeMeta: metav1.TypeMeta{},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "falco-service",
-			Namespace: i.NamespaceName,
+			Name:      serviceName,
+			Namespace: namespace,
 			Labels: map[string]string{
 				"app":  "falco",
 				"role": "security",
@@ -145,30 +167,30 @@ func (i *FalcoInstaller) iFalcoService() error {
 		},
 		Status: v1.ServiceStatus{},
 	}
-	_, err := i.k8s.client.CoreV1().Services(i.NamespaceName).Create(&svc)
+	_, err := serviceClient.Create(svc)
 	if err != nil {
-		return fmt.Errorf("unable to create Falco service %v", err)
+		return err
 	}
 	return nil
 }
 
-func (i *FalcoInstaller) iFalcoRBAC() error {
-	sa := v1.ServiceAccount{
+func createRBAC(serviceAccountClient corev1.ServiceAccountInterface, clusterRoleClient rbacv1beta1.ClusterRoleInterface, clusterRoleBindingClient rbacv1beta1.ClusterRoleBindingInterface, serviceAccountName string, namespace string) error {
+	sa := &v1.ServiceAccount{
 		TypeMeta: metav1.TypeMeta{},
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "falco-account",
+			Name: serviceAccountName,
 			Labels: map[string]string{
 				"app":  "falco",
 				"role": "security",
 			},
 		},
 	}
-	_, err := i.k8s.client.CoreV1().ServiceAccounts(i.NamespaceName).Create(&sa)
+	_, err := serviceAccountClient.Create(sa)
 	if err != nil {
-		return fmt.Errorf("unable to create service account: %v", err)
+		return err
 	}
 
-	cr := v1beta1.ClusterRole{
+	cr := &v1beta1.ClusterRole{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "falco-cluster-role",
 			Labels: map[string]string{
@@ -211,15 +233,16 @@ func (i *FalcoInstaller) iFalcoRBAC() error {
 			},
 		},
 	}
-	_, err = i.k8s.client.RbacV1beta1().ClusterRoles().Create(&cr)
+	_, err = clusterRoleClient.Create(cr)
 	if err != nil {
-		return fmt.Errorf("unable to create cluster role: %v", err)
+		return err
 	}
-	crb := v1beta1.ClusterRoleBinding{
+
+	crb := &v1beta1.ClusterRoleBinding{
 		TypeMeta: metav1.TypeMeta{},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "falco-cluster-role-binding",
-			Namespace: i.NamespaceName,
+			Namespace: namespace,
 			Labels: map[string]string{
 				"app":  "falco",
 				"role": "security",
@@ -228,8 +251,8 @@ func (i *FalcoInstaller) iFalcoRBAC() error {
 		Subjects: []v1beta1.Subject{
 			{
 				Kind:      "ServiceAccount",
-				Name:      "falco-account",
-				Namespace: i.NamespaceName,
+				Name:      serviceAccountName,
+				Namespace: namespace,
 			},
 		},
 		RoleRef: v1beta1.RoleRef{
@@ -238,56 +261,47 @@ func (i *FalcoInstaller) iFalcoRBAC() error {
 			APIGroup: "rbac.authorization.k8s.io",
 		},
 	}
-	_, err = i.k8s.client.RbacV1beta1().ClusterRoleBindings().Create(&crb)
+	_, err = clusterRoleBindingClient.Create(crb)
 	if err != nil {
-		return fmt.Errorf("unable to install cluster role binding: %v", err)
+		return err
 	}
 	return nil
 
 }
 
-func (i *FalcoInstaller) iConfigMap() error {
-	cm := v1.ConfigMap{
+func createConfigMap(configMapClient corev1.ConfigMapInterface, configMapName string, namespace string) error {
+	cm := &v1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "falco-config",
-			Namespace: i.NamespaceName,
+			Name:      configMapName,
+			Namespace: namespace,
 		},
-		Data: defaultFalcoConfig, // see installFalcoDefaultConfig.go
+		Data: defaultFalcoConfig,
 	}
-	_, err := i.k8s.client.CoreV1().ConfigMaps(i.NamespaceName).Create(&cm)
+	_, err := configMapClient.Create(cm)
 	if err != nil {
-		return fmt.Errorf("error insalling configmap: %v", err)
+		return err
 	}
 	return nil
 }
 
-func (i *FalcoInstaller) iNamespace() error {
-	ns := v1.Namespace{
+func createNamespace(namespaceClient corev1.NamespaceInterface, name string) error {
+	ns := &v1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: i.NamespaceName,
+			Name: name,
 		},
 	}
-	_, err := i.k8s.client.CoreV1().Namespaces().Create(&ns)
-	if err != nil {
-		return fmt.Errorf("unable to ensure namespace: %v", err)
-	}
-
-	return nil
+	_, err := namespaceClient.Create(ns)
+	return err
 }
 
-func iDaemonSetWithBPF() {
-
-}
-
-func (i *FalcoInstaller) iDaemonSet(useBPF bool) error {
-	t := true
-	useBPFStr := "PASS"
-	if useBPF == true {
-		useBPFStr = "SYSDIG_BPF_PROBE"
+func createDaemonSet(daemonSetClient appsv1beta2.DaemonSetInterface, serviceAccountName string, configMapName string, daemonSetName string, bpf bool) error {
+	useBPF := "PASS"
+	if bpf == true {
+		useBPF = "SYSDIG_BPF_PROBE"
 	}
-	ds := v1beta2.DaemonSet{
+	ds := &v1beta2.DaemonSet{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "falco-daemonset",
+			Name: daemonSetName,
 			Labels: map[string]string{
 				"app":  "falco",
 				"role": "security",
@@ -308,17 +322,17 @@ func (i *FalcoInstaller) iDaemonSet(useBPF bool) error {
 					},
 				},
 				Spec: v1.PodSpec{
-					ServiceAccountName: "falco-account",
+					ServiceAccountName: serviceAccountName,
 					Containers: []v1.Container{
 						{
 							Name:  "falco",
 							Image: "falcosecurity/falco-slim:0.17.0", // TODO use a specific version not `latest`
 							SecurityContext: &v1.SecurityContext{
-								Privileged: &t,
+								Privileged: pointer.BoolPtr(true),
 							},
 							Env: []v1.EnvVar{
 								{
-									Name:  useBPFStr,
+									Name:  useBPF,
 									Value: "",
 								},
 								{
@@ -352,6 +366,7 @@ func (i *FalcoInstaller) iDaemonSet(useBPF bool) error {
 								{
 									Name:      "dev-fs",
 									MountPath: "/host/dev",
+									// todo > read only?
 								},
 								{
 									Name:      "proc-fs",
@@ -379,13 +394,12 @@ func (i *FalcoInstaller) iDaemonSet(useBPF bool) error {
 									ReadOnly:  true,
 								},
 								{
-									Name:      "falco-config",
+									Name:      configMapName,
 									MountPath: "/etc/falco",
 								},
 							},
 						},
 					},
-
 					Volumes: []v1.Volume{
 						{
 							Name: "docker-socket",
@@ -452,10 +466,10 @@ func (i *FalcoInstaller) iDaemonSet(useBPF bool) error {
 							},
 						},
 						{
-							Name: "falco-config",
+							Name: configMapName,
 							VolumeSource: v1.VolumeSource{
 								ConfigMap: &v1.ConfigMapVolumeSource{
-									LocalObjectReference: v1.LocalObjectReference{Name: "falco-config"},
+									LocalObjectReference: v1.LocalObjectReference{Name: configMapName},
 								},
 							},
 						},
@@ -464,10 +478,9 @@ func (i *FalcoInstaller) iDaemonSet(useBPF bool) error {
 			},
 		},
 	}
-	//fmt.Printf("%+v\n", ds)
-	_, err := i.k8s.client.AppsV1beta2().DaemonSets(i.NamespaceName).Create(&ds)
+	_, err := daemonSetClient.Create(ds)
 	if err != nil {
-		return fmt.Errorf("error with client-go: %v", err)
+		return err
 	}
 	return nil
 }
