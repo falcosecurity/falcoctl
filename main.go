5f1080f08f8f0ccf5f51b1462b2d8647b83d9a6d
Lorenzo Fontana <lo@linux.com>
2019-10-11 13:06:18 +0200

    update: automatic env variables
    Co-Authored-By: Leonardo Di Donato <leodidonato@gmail.com>
    Co-Authored-By: Michael Ducy <michael@ducy.org>
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- main.go
+++ main.go
@@ -19,12 +19,15 @@ import (
 	"os"
 
 	"github.com/falcosecurity/falcoctl/cmd"
+	"github.com/spf13/pflag"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 )
 
 func main() {
+	flags := pflag.NewFlagSet("falcoctl", pflag.ExitOnError)
+	pflag.CommandLine = flags
 	root := cmd.NewRootCommand(genericclioptions.IOStreams{
 		In:     os.Stdin,
 		Out:    os.Stdout,
