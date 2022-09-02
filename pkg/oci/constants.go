797bfef852ac3bd9d9842bc9af04e9dde7795aed
Lorenzo Susini <susinilorenzo1@gmail.com>
2022-09-02 13:58:51 +0000

    new: add constants to the oci pkg
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>
    Co-authored-by: Aldo Lacuku <aldo@lacuku.eu>

---
+++ pkg/oci/constants.go
@@ -0,0 +1,33 @@
+// Copyright 2022 The Falco Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package oci
+
+const (
+
+	// FalcoRuleConfigMediaType is the MediaType for rule's config layer.
+	FalcoRuleConfigMediaType = "application/vnd.cncf.falco.rule.config.v1+json"
+
+	// FalcoRuleLayerMediaType is the MediaType for rules.
+	FalcoRuleLayerMediaType = "application/vnd.cncf.falco.rule.layer.v1+tar.gz"
+
+	// FalcoPluginConfigMediaType is the MediaType for plugin's config layer.
+	FalcoPluginConfigMediaType = "application/vnd.cncf.falco.plugin.config.v1+json"
+
+	// FalcoPluginLayerMediaType is the MediaType for plugins.
+	FalcoPluginLayerMediaType = "application/vnd.cncf.falco.plugin.layer.v1+tar.gz"
+
+	// DefaultRegistry is the default container registry to use.
+	DefaultRegistry = "ghcr.io"
+)
