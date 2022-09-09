81c3c9df23bdd5f3aaa28be106f7bca798bb9104
Aldo Lacuku <aldo@lacuku.eu>
2022-09-09 16:45:06 +0200

    update(pkg/oci/pusher): add support for image index manifest
    Furthermore, the pusher handles the update of plugin artifacts based
    on their archetecture and operating system.
    
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>
    Co-authored-by: Lorenzo Susini <susinilorenzo1@gmail.com>

--- pkg/oci/pusher/pusher.go
+++ pkg/oci/pusher/pusher.go
@@ -17,19 +17,32 @@ package pusher
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"reflect"
+	"strings"
 
+	"github.com/opencontainers/image-spec/specs-go"
 	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 	logger "github.com/sirupsen/logrus"
 	"oras.land/oras-go/v2"
 	"oras.land/oras-go/v2/content/file"
+	"oras.land/oras-go/v2/content/memory"
 	"oras.land/oras-go/v2/registry/remote"
 	"oras.land/oras-go/v2/registry/remote/auth"
 
 	"github.com/falcosecurity/falcoctl/pkg/oci"
 )
+
+var (
+	// ErrNotFound it is wrapped in not found errors returned by the ORAS library.
+	ErrNotFound = errNotFound()
+)
+
+func errNotFound() error { return errors.New("not found") }
 
 // Pusher implements push operations.
 type Pusher struct {
@@ -38,7 +51,7 @@ type Pusher struct {
 }
 
 // NewPusher create a new pusher that can be used for push operations.
-func NewPusher(ctx context.Context, client *auth.Client) (*Pusher, error) {
+func NewPusher(client *auth.Client) (*Pusher, error) {
 	return &Pusher{
 		Client:    client,
 		fileStore: file.New(""),
@@ -51,9 +64,16 @@ func NewPusher(ctx context.Context, client *auth.Client) (*Pusher, error) {
 // ref format follows: REGISTRY/REPO[:TAG|@DIGEST]. Ex. localhost:5000/hello:latest.
 // dependencies rule to plugin dependency expressed as pluginName:version. Ex. cloudtrail:1.2.3.
 func (p *Pusher) Push(ctx context.Context, artifactType oci.ArtifactType,
-	artifactPath, ref string, dependencies ...string) (*oci.RegistryResult, error) {
-	var dataDesc, configDesc, manifestDesc *v1.Descriptor
+	artifactPath, ref, platform string, dependencies ...string) (*oci.RegistryResult, error) {
+	var dataDesc, configDesc, manifestDesc, rootDesc *v1.Descriptor
 	var err error
+
+	// Create the object to interact with the remote repo.
+	repo, err := remote.NewRepository(ref)
+	if err != nil {
+		return nil, err
+	}
+	repo.Client = p.Client
 
 	// Prepare data layer.
 	if dataDesc, err = p.storeMainLayer(ctx, artifactType, artifactPath); err != nil {
@@ -66,31 +86,78 @@ func (p *Pusher) Push(ctx context.Context, artifactType oci.ArtifactType,
 	}
 
 	// Now we can create manifest, using the Config descriptor and principal Layer descriptor.
-	if manifestDesc, err = p.packManifest(ctx, configDesc, dataDesc); err != nil {
+	if manifestDesc, err = p.packManifest(ctx, configDesc, dataDesc, platform); err != nil {
 		return nil, err
 	}
 
-	// Prepare real push operation using the authenticated client.
-	repo, err := remote.NewRepository(ref)
-	if err != nil {
+	if rootDesc, err = p.packIndex(ctx, artifactType, manifestDesc, repo); err != nil {
 		return nil, err
 	}
-	repo.Client = p.Client
 
 	// Tag the manifest desc locally.
-	if err = p.fileStore.Tag(ctx, *manifestDesc, repo.Reference.Reference); err != nil {
+	if err = p.fileStore.Tag(ctx, *rootDesc, repo.Reference.Reference); err != nil {
 		return nil, err
 	}
-
 	_, err = oras.Copy(ctx, p.fileStore, repo.Reference.Reference, repo, "", oras.DefaultCopyOptions)
 	if err != nil {
 		return nil, err
 	}
 
-	// Todo(loresuso, alacuku): not sure what to return. The manifest itself could be enough since it holds useful data.
 	return &oci.RegistryResult{
 		Digest: string(manifestDesc.Digest),
 	}, nil
+}
+
+func (p *Pusher) retrieveIndex(ctx context.Context, repo *remote.Repository) (*v1.Index, error) {
+	var indexBytes []byte
+	var index v1.Index
+
+	memoryStore := memory.New()
+	ref := repo.Reference.String()
+	indexDesc, err := oras.Copy(ctx, repo, repo.Reference.Reference, memoryStore, "", oras.DefaultCopyOptions)
+	if err != nil {
+		if strings.Contains(err.Error(), fmt.Sprintf("%s: not found", repo.Reference.Reference)) {
+			return nil, fmt.Errorf("unable to download image index for ref %s, %w", ref, ErrNotFound)
+		}
+		return nil, fmt.Errorf("unable to download image index for ref %s: %w", ref, err)
+	}
+
+	// Check if the descriptor has media type image index.
+	if indexDesc.MediaType != v1.MediaTypeImageIndex {
+		return nil, fmt.Errorf("the pulled descriptor for ref %q has media type %q while expecting %q",
+			ref, indexDesc.MediaType, v1.MediaTypeImageIndex)
+	}
+
+	reader, err := memoryStore.Fetch(ctx, indexDesc)
+	if err != nil {
+		return nil, fmt.Errorf("unable to fetch index from memory store for ref %q: %w", ref, err)
+	}
+
+	if indexBytes, err = io.ReadAll(reader); err != nil {
+		return nil, fmt.Errorf("unable to read index from reader for ref %q: %w", ref, err)
+	}
+
+	if err = json.Unmarshal(indexBytes, &index); err != nil {
+		return nil, fmt.Errorf("unable to unmarshal index for ref %q: %w", ref, err)
+	}
+	return &index, nil
+}
+
+func (p *Pusher) updateIndex(indexDesc *v1.Index, manifestDesc *v1.Descriptor) *v1.Index {
+	// Check if the index already contains the manifest for the given platform.
+	for i, m := range indexDesc.Manifests {
+		// If we find a manifest in the index that has the same platform as the artifact that we are currently
+		// processing it means that we are going to overwrite it with the current version. Only if the digests are
+		// different.
+		if reflect.DeepEqual(m.Platform, manifestDesc.Platform) {
+			// Remove manifest from the index.
+			indexDesc.Manifests = append(indexDesc.Manifests[:i], indexDesc.Manifests[i+1:]...)
+			break
+		}
+	}
+
+	indexDesc.Manifests = append(indexDesc.Manifests, *manifestDesc)
+	return indexDesc
 }
 
 func (p *Pusher) storeMainLayer(ctx context.Context, artifactType oci.ArtifactType, artifactPath string) (*v1.Descriptor, error) {
@@ -127,9 +194,13 @@ func (p *Pusher) storeConfigLayer(ctx context.Context, artifactType oci.Artifact
 		layerMediaType = oci.FalcoPluginConfigMediaType
 	}
 
-	configData, err := json.Marshal(artifactConfig)
+	return p.toFileStore(ctx, layerMediaType, "config", artifactConfig)
+}
+
+func (p *Pusher) toFileStore(ctx context.Context, mediaType, name string, data interface{}) (*v1.Descriptor, error) {
+	dataBytes, err := json.Marshal(data)
 	if err != nil {
-		return nil, fmt.Errorf("unable to marshal artifact config %v: %w", artifactConfig, err)
+		return nil, fmt.Errorf("unable to marshal data of media type %q: %w", mediaType, err)
 	}
 
 	// Create temporary common file. This is needed because we have to add it to the store.
@@ -143,19 +214,18 @@ func (p *Pusher) storeConfigLayer(ctx context.Context, artifactType oci.Artifact
 		}
 	}()
 
-	if _, err := configFile.Write(configData); err != nil {
-		return nil, fmt.Errorf("unable to write config file to temporary file %s: %w", configFile.Name(), err)
+	if _, err := configFile.Write(dataBytes); err != nil {
+		return nil, fmt.Errorf("unable to write data of media type %q to temporary file %s: %w", mediaType, configFile.Name(), err)
 	}
 
-	desc, err := p.fileStore.Add(ctx, "config", layerMediaType, filepath.Clean(configFile.Name()))
+	desc, err := p.fileStore.Add(ctx, name, mediaType, filepath.Clean(configFile.Name()))
 	if err != nil {
-		return nil, fmt.Errorf("unable to store artifact %s of type %s: %w", configFile.Name(), artifactType, err)
+		return nil, fmt.Errorf("unable to store data of media type %q in the file store: %w", mediaType, err)
 	}
-
 	return &desc, nil
 }
 
-func (p *Pusher) packManifest(ctx context.Context, configDesc, dataDesc *v1.Descriptor) (*v1.Descriptor, error) {
+func (p *Pusher) packManifest(ctx context.Context, configDesc, dataDesc *v1.Descriptor, platform string) (*v1.Descriptor, error) {
 	// Now we can create manifest, using the Config descriptor and principal Layer descriptor.
 	packOptions := oras.PackOptions{ConfigDescriptor: configDesc}
 	desc, err := oras.Pack(ctx, p.fileStore, []v1.Descriptor{*dataDesc}, packOptions)
@@ -163,5 +233,44 @@ func (p *Pusher) packManifest(ctx context.Context, configDesc, dataDesc *v1.Desc
 		return nil, fmt.Errorf("unable to generate manifest for config layer %s and data layer %s: %w", configDesc.MediaType, dataDesc.MediaType, err)
 	}
 
+	if dataDesc.MediaType == oci.FalcoPluginLayerMediaType {
+		tokens := strings.Split(platform, "/")
+		desc.Platform = &v1.Platform{
+			OS:           tokens[0],
+			Architecture: tokens[1],
+		}
+	}
+
 	return &desc, nil
+}
+
+func (p *Pusher) packIndex(ctx context.Context, artifactType oci.ArtifactType,
+	manifestDesc *v1.Descriptor, repo *remote.Repository) (*v1.Descriptor, error) {
+	ref := repo.Reference.String()
+
+	// If we are handling an artifact of type "plugin" then we need to pull the image
+	// index (https://github.com/opencontainers/image-spec/blob/main/image-index.md)
+	if artifactType == oci.Plugin {
+		index, err := p.retrieveIndex(ctx, repo)
+		// In case of error return.
+		if err != nil && !errors.Is(err, ErrNotFound) {
+			return nil, fmt.Errorf("unable to retrieve index with ref %q from remote repo: %w", ref, err)
+		}
+
+		// If not index is present than create one.
+		if errors.Is(err, ErrNotFound) {
+			index = &v1.Index{
+				Versioned: specs.Versioned{SchemaVersion: 2},
+				MediaType: v1.MediaTypeImageIndex,
+			}
+		}
+
+		index = p.updateIndex(index, manifestDesc)
+		newIndexDesc, err := p.toFileStore(ctx, v1.MediaTypeImageIndex, "index", index)
+		if err != nil {
+			return nil, fmt.Errorf("unable to add index content to the file store: %w", err)
+		}
+		return newIndexDesc, nil
+	}
+	return manifestDesc, nil
 }
