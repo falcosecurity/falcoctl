790b378445208adea8bae71acda6dcd4e48e385e
Lorenzo Susini <susinilorenzo1@gmail.com>
2022-09-16 15:17:28 +0000

    new(pkg): create a library to manage indexes
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>

---
+++ pkg/index/index.go
@@ -0,0 +1,172 @@
+// Copyright 2022 The Falco Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package index
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+// Entry describes an entry of the index stored remotely and cached locally.
+type Entry struct {
+	Description string   `yaml:"description"`
+	Home        string   `yaml:"home"`
+	Keywords    []string `yaml:"keywords"`
+	License     string   `yaml:"license"`
+	Maintainers []struct {
+		Email string `yaml:"email"`
+		Name  string `yaml:"name"`
+	} `yaml:"maintainers"`
+	Name       string   `yaml:"name"`
+	Registry   string   `yaml:"registry"`
+	Repository string   `yaml:"repository"`
+	Sources    []string `yaml:"sources"`
+	Type       string   `yaml:"type"`
+}
+
+// Index represents an index.
+type Index struct {
+	Name        string
+	Filename    string
+	Entries     []*Entry
+	entryByName map[string]*Entry
+}
+
+// MergedIndexes is used to aggregate all indexes and perform search operations.
+type MergedIndexes struct {
+	Index
+	indexByEntry map[*Entry]*Index
+}
+
+// NewIndex loads an Index from a file.
+func NewIndex(path, name string) (*Index, error) {
+	indexBytes, err := os.ReadFile(filepath.Clean(path))
+	if err != nil {
+		return nil, fmt.Errorf("cannot read file %s: %w", path, err)
+	}
+
+	var index Index
+	var entries []Entry
+	if err = yaml.Unmarshal(indexBytes, &entries); err != nil {
+		return nil, fmt.Errorf("cannot unmarshal index: %w", err)
+	}
+
+	for k := range entries {
+		index.Entries = append(index.Entries, &entries[k])
+	}
+
+	index.Name = name
+	index.Filename = path
+
+	return &index, nil
+}
+
+// Upsert adds a new entry to the Index or updates an existing one.
+func (i *Index) Upsert(entry *Entry) {
+	defer func() {
+		i.entryByName[entry.Name] = entry
+	}()
+
+	for k, e := range i.Entries {
+		if e.Name == entry.Name {
+			i.Entries[k] = entry
+			return
+		}
+	}
+
+	i.Entries = append(i.Entries, entry)
+}
+
+// Remove removes an entry from the Index.
+func (i *Index) Remove(entry *Entry) error {
+	for k, e := range i.Entries {
+		if e == entry {
+			i.Entries = append(i.Entries[:k], i.Entries[k+1:]...)
+			delete(i.entryByName, e.Name)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("cannot remove %s: not found", entry.Name)
+}
+
+// EntryByName returns a Entry by passing its name.
+func (i *Index) EntryByName(name string) *Entry {
+	return i.entryByName[name]
+}
+
+// Write writes an Index to disk.
+func (i *Index) Write(path string) error {
+	indexBytes, err := yaml.Marshal(i.Entries)
+	if err != nil {
+		return fmt.Errorf("cannot marshal index: %w", err)
+	}
+
+	if err = os.WriteFile(path, indexBytes, writePermissions); err != nil {
+		return fmt.Errorf("cannot write index to file: %w", err)
+	}
+
+	return nil
+}
+
+// NewMergedIndexes initializes a MergedIndex.
+func NewMergedIndexes() *MergedIndexes {
+	m := &MergedIndexes{}
+
+	m.entryByName = make(map[string]*Entry)
+	m.indexByEntry = make(map[*Entry]*Index)
+
+	return m
+}
+
+// Merge creates a new index by merging all the indexes that are passed.
+// Orders matters. Be sure to pass an ordered list of indexes. For our use case, sort by added time.
+func (m *MergedIndexes) Merge(indexes ...*Index) {
+	for _, index := range indexes {
+		for _, Entry := range index.Entries {
+			m.Upsert(Entry)
+			m.indexByEntry[Entry] = index
+		}
+	}
+}
+
+// SearchByKeywords search for entries matching the given keywords in MergedIndexes.
+func (m *MergedIndexes) SearchByKeywords(keywords ...string) []*Entry {
+	var result []*Entry
+	keywordSet := make(map[string]bool)
+
+	for _, keyword := range keywords {
+		keywordSet[keyword] = true
+	}
+
+	for _, entry := range m.Entries {
+		for _, indexKeyword := range entry.Keywords {
+			if _, ok := keywordSet[indexKeyword]; ok {
+				result = append(result, entry)
+				break
+			}
+		}
+	}
+
+	return result
+}
+
+// IndexByEntry is used to retrieve the original index from an entry in MergedIndexes.
+func (m *MergedIndexes) IndexByEntry(entry *Entry) *Index {
+	return m.indexByEntry[entry]
+}
