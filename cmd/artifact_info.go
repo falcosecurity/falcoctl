2bfe0d0934271accce2ca31ced746a0aabe7c7d8
Lorenzo Susini <susinilorenzo1@gmail.com>
2022-10-04 10:14:46 +0000

    new(cmd): introduce artifact info command
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>

---
+++ cmd/artifact_info.go
@@ -0,0 +1,115 @@
+// Copyright 2022 The Falco Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/falcosecurity/falcoctl/cmd/internal/utils"
+	"github.com/falcosecurity/falcoctl/pkg/index"
+	"github.com/falcosecurity/falcoctl/pkg/oci"
+	"github.com/falcosecurity/falcoctl/pkg/oci/authn"
+	"github.com/falcosecurity/falcoctl/pkg/options"
+	"github.com/falcosecurity/falcoctl/pkg/output"
+	"github.com/spf13/cobra"
+	"oras.land/oras-go/v2/registry"
+)
+
+type artifactInfoOptions struct {
+	*options.CommonOptions
+}
+
+// NewArtifactInfoCmd returns the artifact info command.
+func NewArtifactInfoCmd(ctx context.Context, opt *options.CommonOptions) *cobra.Command {
+	o := artifactInfoOptions{
+		CommonOptions: opt,
+	}
+
+	cmd := &cobra.Command{
+		Use:                   "info [ref1 [ref2 ...]] [flags]",
+		DisableFlagsInUseLine: true,
+		Short:                 "Retrieve all available versions of a given artifact",
+		Long:                  "Retrieve all available versions of a given artifact",
+		Args:                  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			o.Printer.CheckErr(o.RunArtifactInfo(ctx, args))
+		},
+	}
+
+	return cmd
+}
+
+func (o *artifactInfoOptions) RunArtifactInfo(ctx context.Context, args []string) error {
+	indexConfig, err := index.NewConfig(indexesFile)
+	if err != nil {
+		return err
+	}
+
+	mergedIndexes, err := utils.Indexes(indexConfig, falcoctlPath)
+	if err != nil {
+		return err
+	}
+
+	credentialStore, err := authn.NewStore([]string{}...)
+	if err != nil {
+		return err
+	}
+
+	var data [][]string
+	for _, name := range args {
+		var ref string
+		parsedRef, err := registry.ParseReference(name)
+		if err != nil {
+			entry, ok := mergedIndexes.EntryByName(name)
+			if !ok {
+				o.Printer.Warning.Printfln("cannot find %q, skipping", name)
+				continue
+			}
+			ref = fmt.Sprintf("%s/%s", entry.Registry, entry.Repository)
+		} else {
+			parsedRef.Reference = ""
+			ref = parsedRef.String()
+		}
+
+		registry, err := utils.GetRegistryFromRef(ref)
+		if err != nil {
+			return err
+		}
+
+		cred, err := credentialStore.Credential(ctx, registry)
+		if err != nil {
+			return err
+		}
+
+		client := authn.NewClient(cred)
+
+		tags, err := oci.Tags(ctx, ref, client)
+		if err != nil {
+			o.Printer.Warning.Printfln("cannot retrieve tags from %q", ref)
+			continue
+		}
+
+		joinedTags := strings.Join(tags[:], " ")
+		data = append(data, []string{ref, joinedTags})
+	}
+
+	if err = o.Printer.PrintTable(output.ArtifactInfo, data); err != nil {
+		return err
+	}
+
+	return nil
+}
