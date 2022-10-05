7c4225807c19e759673ea43b4669cb24dd2403a6
Lorenzo Susini <susinilorenzo1@gmail.com>
2022-10-05 10:59:29 +0000

    new(pkg): introduce functions to compute levenshtein distance and score
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>

--- pkg/index/index.go
+++ pkg/index/index.go
@@ -16,9 +16,11 @@ package index
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"path/filepath"
 	"sort"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -211,4 +213,54 @@ func (i *Index) SearchByKeywords(keywords ...string) []*Entry {
 // IndexByEntry is used to retrieve the original index from an entry in MergedIndexes.
 func (m *MergedIndexes) IndexByEntry(entry *Entry) *Index {
 	return m.indexByEntry[entry]
+}
+
+// levenshteinDistance computes the edit distance between two strings.
+func levenshteinDistance(s, t string) int {
+	s = strings.ToLower(s)
+	t = strings.ToLower(t)
+
+	d := make([][]int, len(s)+1)
+
+	for i := range d {
+		d[i] = make([]int, len(t)+1)
+	}
+
+	for i := range d {
+		d[i][0] = i
+	}
+
+	for j := range d[0] {
+		d[0][j] = j
+	}
+
+	for j := 1; j <= len(t); j++ {
+		for i := 1; i <= len(s); i++ {
+			if s[i-1] == t[j-1] {
+				d[i][j] = d[i-1][j-1]
+			} else {
+				min := d[i-1][j]
+				if d[i][j-1] < min {
+					min = d[i][j-1]
+				}
+				if d[i-1][j-1] < min {
+					min = d[i-1][j-1]
+				}
+				d[i][j] = min + 1
+			}
+		}
+	}
+
+	return d[len(s)][len(t)]
+}
+
+func score(s, t string) float64 {
+	distance := levenshteinDistance(s, t)
+
+	longerLen := math.Max(float64(len(s)), float64(len(t)))
+
+	// The maximum levenshtein distance between two strings is equal
+	// to the length of the longer string. We can use this to compute
+	// a ratio.
+	return (longerLen - float64(distance)) / longerLen
 }
