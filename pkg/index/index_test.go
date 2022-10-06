c02ccc26c433735b3f03558687ac6cd3da6062cb
Lorenzo Susini <susinilorenzo1@gmail.com>
2022-10-06 16:38:09 +0000

    test(pkg/index): add tests on index related ops
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>

--- pkg/index/index_test.go
+++ pkg/index/index_test.go
@@ -44,6 +44,138 @@ const expectedIndexNormalized = `- name: baz
   sources: []
 `
 
+func TestUpsert(t *testing.T) {
+	i := New("name")
+
+	i.Upsert(&Entry{
+		Name: "foo",
+		Type: "plugin",
+	})
+
+	if len(i.Entries) != 1 {
+		t.Errorf("error upsert in index")
+	}
+
+	i.Upsert(&Entry{
+		Name: "foo",
+		Type: "bar",
+	})
+
+	entry, ok := i.EntryByName("foo")
+	if !ok {
+		t.Errorf("cannot retrieve entry by name")
+	}
+
+	if entry.Type != "bar" {
+		t.Errorf("upsert didn't modify the entry")
+	}
+}
+
+func TestRemove(t *testing.T) {
+	i := New("name")
+
+	entry := &Entry{
+		Name: "foo",
+		Type: "plugin",
+	}
+
+	i.Upsert(entry)
+
+	if err := i.Remove(entry); err != nil {
+		t.Error(err)
+	}
+
+	if len(i.Entries) != 0 {
+		t.Errorf("error remove from index")
+	}
+}
+
+func TestMerge(t *testing.T) {
+	i1 := New("index1")
+	i2 := New("index2")
+
+	i1.Upsert(&Entry{
+		Name: "cloudtrail",
+		Type: "plugin",
+	})
+
+	i1.Upsert(&Entry{
+		Name: "github",
+		Type: "plugin",
+	})
+
+	i2.Upsert(&Entry{
+		Name: "okta",
+		Type: "plugin",
+	})
+
+	mergedIndex := NewMergedIndexes()
+	mergedIndex.Merge(i1, i2)
+
+	if len(mergedIndex.Entries) != 3 {
+		t.Errorf("Indexes not properly merged")
+	}
+
+	cloudtrail, ok := mergedIndex.EntryByName("cloudtrail")
+	if !ok {
+		t.Errorf("cannot retrieve entry from merged index")
+	}
+
+	okta, ok := mergedIndex.EntryByName("okta")
+	if !ok {
+		t.Errorf("cannot retrieve entry from merged index")
+	}
+
+	if mergedIndex.IndexByEntry(cloudtrail).Name != "index1" ||
+		mergedIndex.IndexByEntry(okta).Name != "index2" {
+		t.Errorf("cannot correctly retireve original index from merged index")
+	}
+}
+
+func TestSearchByKeywords(t *testing.T) {
+	i := New("name")
+
+	i.Upsert(&Entry{
+		Name: "cloudtrail",
+		Type: "plugin",
+	})
+
+	// Test exact name.
+	exact_name := i.SearchByKeywords(1, "cloudtrail")
+	if len(exact_name) != 1 {
+		t.Errorf("error in SearchByKeywords, expected to find one exact match")
+	}
+
+	// Test mistyped name.
+	mistyped_name := i.SearchByKeywords(0.8, "cloudtrailz")
+	if len(mistyped_name) != 1 {
+		t.Errorf("error in SearchByKeywords, expected to find a match even if keyword is mistyped")
+	}
+
+	// Test with perfect match on keyword.
+	i.Upsert(&Entry{
+		Name:     "github",
+		Keywords: []string{"webhook", "security", "audit"},
+	})
+	perfect_keyword_match := i.SearchByKeywords(1, "webhook")
+	if len(perfect_keyword_match) != 1 {
+		t.Errorf("error in SearchByKeywords, expected to find a perfect match with keyword")
+	}
+
+	// Test partial match
+	partial_keyword_match := i.SearchByKeywords(1, "web")
+	if len(partial_keyword_match) != 1 {
+		t.Errorf("error in SearchByKeywords, expected to find a partial match with keyword")
+	}
+
+	// Check that no duplicates are returned
+	no_duplicates := i.SearchByKeywords(1, "github", "webhook")
+	if len(no_duplicates) != 1 {
+		t.Errorf("error in SearchByKeywords, not expecting duplicates")
+	}
+
+}
+
 func TestNormalize(t *testing.T) {
 	i := Index{
 		Name:        "name",
