654ff63bd7bf8c28ab6056562e31d028ce005544
Lorenzo Susini <susinilorenzo1@gmail.com>
2023-01-16 14:05:12 +0000

    update: make artifact commands use a configuration file to enable feed feature
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>
    Co-authored-by: Aldo Lacuku <aldo@lacuku.eu>

--- cmd/artifact.go
+++ cmd/artifact.go
@@ -18,12 +18,18 @@ import (
 	"context"
 
 	"github.com/spf13/cobra"
+	"golang.org/x/oauth2/clientcredentials"
+	"oras.land/oras-go/v2/registry/remote/auth"
 
 	"github.com/falcosecurity/falcoctl/internal/artifact/follow"
 	"github.com/falcosecurity/falcoctl/internal/artifact/info"
 	"github.com/falcosecurity/falcoctl/internal/artifact/install"
 	"github.com/falcosecurity/falcoctl/internal/artifact/list"
 	"github.com/falcosecurity/falcoctl/internal/artifact/search"
+	"github.com/falcosecurity/falcoctl/internal/config"
+	"github.com/falcosecurity/falcoctl/internal/index/add"
+	"github.com/falcosecurity/falcoctl/internal/registry/login"
+	"github.com/falcosecurity/falcoctl/internal/registry/oauth"
 	commonoptions "github.com/falcosecurity/falcoctl/pkg/options"
 )
 
@@ -34,6 +40,48 @@ func NewArtifactCmd(ctx context.Context, opt *commonoptions.CommonOptions) *cobr
 		DisableFlagsInUseLine: true,
 		Short:                 "Interact with Falco artifacts",
 		Long:                  "Interact with Falco artifacts",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			opt.Initialize()
+			opt.Printer.CheckErr(config.Load(opt.ConfigFile))
+
+			// add indexes if needed
+			// Set up basic authentication
+			indexes, err := config.Indexes()
+			opt.Printer.CheckErr(err)
+
+			for _, ind := range indexes {
+				indexMgr := add.IndexAddOptions{
+					CommonOptions: opt,
+				}
+				opt.Printer.CheckErr(indexMgr.Validate([]string{ind.Name, ind.URL}))
+				opt.Printer.CheckErr(indexMgr.RunIndexAdd(ctx, []string{ind.Name, ind.URL}))
+			}
+
+			basicAuths, err := config.BasicAuths()
+			opt.Printer.CheckErr(err)
+			for _, basicAuth := range basicAuths {
+				cred := &auth.Credential{
+					Username: basicAuth.User,
+					Password: basicAuth.Password,
+				}
+
+				opt.Printer.CheckErr(login.DoLogin(ctx, basicAuth.Registry, cred))
+			}
+
+			oauthAuths, err := config.OauthAuths()
+			opt.Printer.CheckErr(err)
+			for _, auth := range oauthAuths {
+				oauthMgr := oauth.RegistryOauthOptions{
+					CommonOptions: opt,
+					Conf: clientcredentials.Config{
+						ClientID:     auth.ClientID,
+						ClientSecret: auth.ClientSecret,
+						TokenURL:     auth.TokenURL,
+					},
+				}
+				opt.Printer.CheckErr(oauthMgr.RunOauth(ctx, []string{auth.Registry}))
+			}
+		},
 	}
 
 	cmd.AddCommand(search.NewArtifactSearchCmd(ctx, opt))
