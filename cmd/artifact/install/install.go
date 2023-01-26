8e5986948d43c889cb7000244660915de9c33683
Roberto Scolaro <roberto.scolaro21@gmail.com>
2023-01-26 13:59:05 +0000

    fix(cmd): correctly handle install args
    Signed-off-by: Roberto Scolaro <roberto.scolaro21@gmail.com>

--- cmd/artifact/install/install.go
+++ cmd/artifact/install/install.go
@@ -91,12 +91,12 @@ func NewArtifactInstallCmd(ctx context.Context, opt *options.CommonOptions) *cob
 
 // RunArtifactInstall executes the business logic for the artifact install command.
 func (o *artifactInstallOptions) RunArtifactInstall(ctx context.Context, args []string) error {
-	if len(args) == 0 {
-		configuredInstaller, err := config.Installer()
-		if err != nil {
-			o.Printer.CheckErr(fmt.Errorf("unable to retrieved the configured installer: %w", err))
-		}
+	configuredInstaller, err := config.Installer()
+	if err != nil {
+		o.Printer.CheckErr(fmt.Errorf("unable to retrieved the configured installer: %w", err))
+	}
 
+	if len(args) == 0 {
 		// Set args as configured if no arg was passed
 		if len(configuredInstaller.Artifacts) == 0 {
 			return fmt.Errorf("no artifacts to install, please configure artifacts or pass them as arguments to this command")
