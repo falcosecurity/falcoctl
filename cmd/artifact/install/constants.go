7ae72abd5455d7508e6caadc21e0b7198dd4cad3
Massimiliano Giovagnoli <me@maxgio.it>
2023-02-28 20:23:16 +0100

    chore(cmd/artficat/install): document flag names
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- cmd/artifact/install/constants.go
+++ cmd/artifact/install/constants.go
@@ -1,8 +1,30 @@
+// Copyright 2023 The Falco Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package install
 
 const (
-	FlagRulesFilesDir   = "rulesfiles-dir"
+
+	// FlagRulesFilesDir is the name of the flag to specify the directory path of the rules files.
+	FlagRulesFilesDir = "rulesfiles-dir"
+
+	// FlagPluginsFilesDir is the name of the flag to specify the directory path of the plugins.
 	FlagPluginsFilesDir = "plugins-dir"
-	FlagAllowedTypes    = "allowed-types"
-	FlagResolveDeps     = "resolve-deps"
+
+	// FlagAllowedTypes is the name of the flag to specify allowed artifact types.
+	FlagAllowedTypes = "allowed-types"
+
+	// FlagResolveDeps is the name of the flag to enable artifact dependencies resolution.
+	FlagResolveDeps = "resolve-deps"
 )
