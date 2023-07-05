363a4dca76db71b474c72193554221284ae79ff8
Mickaël Fontès <81414455+MickaelFontes@users.noreply.github.com>
2023-07-05 17:33:40 +0200

    :memo: Update examples and documentation
    * Update README.md with examples
    * Update gcp command help
    * Set registry username as constant for GCP auth
    
    Signed-off-by: Mickaël Fontès <81414455+MickaelFontes@users.noreply.github.com>

--- pkg/oci/authn/gcp.go
+++ pkg/oci/authn/gcp.go
@@ -26,6 +26,12 @@ import (
 	"github.com/falcosecurity/falcoctl/internal/config"
 )
 
+const (
+	// UsernameAccessToken is the valid username for Artifact Registry authentication with an access token
+	// See https://cloud.google.com/artifact-registry/docs/docker/authentication#token
+	UsernameAccessToken = "oauth2accesstoken"
+)
+
 var (
 	// SavedTokenSource saved for all registries using gcp credentials.
 	SavedTokenSource oauth2.TokenSource
@@ -50,10 +56,10 @@ func GCPCredential(ctx context.Context, reg string) (auth.Credential, error) {
 	if SavedTokenSource == nil {
 		tokenSource, err = google.DefaultTokenSource(ctx)
 		if err != nil {
-			return auth.EmptyCredential, fmt.Errorf("wrong gcp source, unable to find a valid source: %w", err)
+			return auth.EmptyCredential, fmt.Errorf("error while trying to identify a GCP TokenSource %w", err)
 		}
 		if tokenSource == nil {
-			return auth.EmptyCredential, fmt.Errorf("unable to retrieve gcp credentials from identified source %w", err)
+			return auth.EmptyCredential, fmt.Errorf("wrong GCP source, unable to find a valid TokenSource: %w", err)
 		}
 		tokenSource = oauth2.ReuseTokenSource(nil, tokenSource)
 		SavedTokenSource = tokenSource
@@ -67,7 +73,7 @@ func GCPCredential(ctx context.Context, reg string) (auth.Credential, error) {
 	}
 
 	return auth.Credential{
-		Username: "oauth2accesstoken",
+		Username: UsernameAccessToken,
 		Password: token.AccessToken,
 	}, nil
 }
