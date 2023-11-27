d762a9c6f7fc28b4b6d0d9a7dddba776b56bc5d5
Federico Di Pierro <nierro92@gmail.com>
2023-11-27 11:15:44 +0100

    chore(cmd/driver): updated driver config long description.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>
    
    Co-authored-by: Aldo Lacuku <aldo@lacuku.eu>

--- cmd/driver/config/config.go
+++ cmd/driver/config/config.go
@@ -43,6 +43,11 @@ import (
 
 const (
 	configMapEngineKindKey = "engine.kind"
+	longConfig             = `Configure a driver for future usages with other driver subcommands.
+It will also update local Falco configuration or k8s configmap depending on the environment where it is running, to let Falco use chosen driver.
+Only supports deployments of Falco that use a driver engine, ie: one between kmod, ebpf and modern-ebpf.
+If engine.kind key is set to a non-driver driven engine, Falco configuration won't be touched.
+`
 )
 
 type driverConfigOptions struct {
@@ -68,7 +73,7 @@ func NewDriverConfigCmd(ctx context.Context, opt *options.Common) *cobra.Command
 		Use:                   "config [flags]",
 		DisableFlagsInUseLine: true,
 		Short:                 "Configure a driver",
-		Long:                  "Configure a driver for future usages with other driver subcommands",
+		Long:                  longConfig,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return o.RunDriverConfig(ctx, cmd)
 		},
