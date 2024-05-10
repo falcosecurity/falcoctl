0469ee9353cc3fdbede56b08ecb419295f6a89fa
Federico Di Pierro <nierro92@gmail.com>
2024-05-10 15:34:50 +0200

    chore(pkg/distro): driverkit does validate env now.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- pkg/driver/distro/distro.go
+++ pkg/driver/distro/distro.go
@@ -187,10 +187,6 @@ func Build(ctx context.Context,
 		return "", err
 	}
 
-	if env == nil {
-		env = make(map[string]string)
-	}
-
 	// Disable automatic kernel headers fetching
 	// if customizeBuild already retrieved kernel headers for us
 	// (and has set the KernelDirEnv key)
